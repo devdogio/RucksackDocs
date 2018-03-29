911aa235c3180b1563d8dde22ea001327c2217eb
Joris Huijbregts <jjahuijbregts@gmail.com>
2018-03-29 11:38:44 +0200

    Removed useless files and test with conf.py

---
+++ conf.py
@@ -0,0 +1,13 @@
+html_theme_options = {
+    'analytics_id': '',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 3,
+    'includehidden': True,
+    'titles_only': False
+}
